1c06d115dc7fe8a5839f64999bc759137acc6ad6
elena-du <39673440+elena-du@users.noreply.github.com>
2019-02-20 15:47:08 -0500

    draft_Elena

--- app/server.R
+++ app/server.R
@@ -1,11 +1,13 @@
-#load("./output/processed_data_map.rds")
+#data = read.csv("processed_data_map.csv")
 function(input, output) {
   
   map_1 <- leaflet() %>%
     addTiles() %>%
     setView(lng = 360 - 95,
             lat = 40,
-            zoom = 4) 
+            zoom = 4) %>%
+    addMarkers(lng = data$LONGITUDE,
+               lat = data$LATITUDE)
   output$map_1 <- renderLeaflet(map_1)
   
   # u.list <- reactive({if (input$add == 0) return() isolate({
@@ -13,6 +15,8 @@ function(input, output) {
   # })})
   # 
   # output
+  
+  
   
 }
 
